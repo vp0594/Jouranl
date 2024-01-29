6f7663e96199db63844347ce7a944f8aa3461324
VP <vatsalpatel999999@gmailcom>
2024-01-29 17:02:45 +0530

    Completed activity_entry.xml

--- app/src/main/java/com/example/joranl/ListFragment.java
+++ app/src/main/java/com/example/joranl/ListFragment.java
@@ -1,17 +1,19 @@
 package com.example.joranl;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class ListFragment extends Fragment {
 
+    private FloatingActionButton addEntryFAB;
 
     public ListFragment() {
     }
@@ -19,9 +21,11 @@ public class ListFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-
         View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        addEntryFAB = view.findViewById(R.id.addEntryFAB);
+
+        addEntryFAB.setOnClickListener(v -> startActivity(new Intent(getActivity(), EntryActivity.class)));
 
 
         return view;
